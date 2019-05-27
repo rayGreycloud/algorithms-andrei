ed54a9200237c6c03aa12c368952c96d7d8ed5bf
rayGreycloud <rayGreycloud@gmail.com>
2019-05-27 11:06:56 -0700

    BST with insert

---
+++ binary-search-tree.js
@@ -0,0 +1,65 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          // Left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  lookup(value) {}
+}
+
+function traverse(node) {
+  if (!node) return null;
+
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
+const myTree = new BinarySearchTree();
+myTree.insert(9);
+myTree.insert(4);
+myTree.insert(6);
+myTree.insert(20);
+myTree.insert(170);
+myTree.insert(15);
+myTree.insert(1);
+
+const resultTree = traverse(myTree.root);
+
+console.log(resultTree);
