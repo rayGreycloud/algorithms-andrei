49090e766377593d44c873cf6c699630acbceafa
rayGreycloud <rayGreycloud@gmail.com>
2019-06-04 11:18:34 -0700

    add comments plus small refactor

--- insertionSort.js
+++ insertionSort.js
@@ -2,12 +2,17 @@ function insertionSort(array) {
   let currentVal;
 
   for (let i = 1; i < array.length; i++) {
+    // track current value
     currentVal = array[i];
     let j;
 
+    // check previous values
+    // if greater than currentVal, move that value
     for (j = i - 1; j >= 0 && array[j] > currentVal; j--) {
       array[j + 1] = array[j];
     }
+    // loop ends when new location of currentVal found
+    // place value into new location
     array[j + 1] = currentVal;
   }
   return array;
@@ -15,8 +20,7 @@ function insertionSort(array) {
 
 // Andrei Solution
 function insertionSort2(array) {
-  const length = array.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i] < array[0]) {
       //move number to the first position
       array.unshift(array.splice(i, 1)[0]);
@@ -24,7 +28,7 @@ function insertionSort2(array) {
       // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
       if (array[i] < array[i - 1]) {
         //find where number should go
-        for (var j = 1; j < i; j++) {
+        for (let j = 1; j < i; j++) {
           if (array[i] >= array[j - 1] && array[i] < array[j]) {
             //move number to the right spot
             array.splice(j, 0, array.splice(i, 1)[0]);
